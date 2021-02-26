2ab6d93e3e4f1ee524d91a18b993578852ca13f4
dantunesd <diney.antunes@catho.com>
2021-02-26 18:51:51 -0300

    feat: 🎸 initialize a basic webserver

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+require('dotenv').config();
+
+const express = require('express');
+const enviroment = require('./infrastructure/enviroment');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(enviroment.APP_PORT, () => {
+  console.log(`API listening at http://localhost:${enviroment.APP_PORT}`);
+});
