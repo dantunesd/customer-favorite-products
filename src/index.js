688d6f332577492607944aeeedddc1f901e7ff65
dantunesd <diney.antunes@catho.com>
2021-02-26 19:08:14 -0300

    feat: 🎸 move express routes to router folder

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const enviroment = require('./infrastructure/enviroment');
+const customers = require('./routers/customers');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/customers', customers);
 
 app.listen(enviroment.APP_PORT, () => {
   console.log(`API listening at http://localhost:${enviroment.APP_PORT}`);
