7d23fa5c6ff471c1748a72ae40a10ba09ab9b73b
dantunesd <diney.antunes@catho.com>
2021-02-26 22:25:09 -0300

    feat: 🎸 make a simple body validation

--- src/routers/customers.js
+++ src/routers/customers.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const validateSchema = require('../infrastructure/schemaValidator');
+const customerPutContent = require('../schemas/customerPutContent.json');
 
 const router = express.Router();
 
-router.put('/:customerId?', (req, res) => {
+router.put('/:customerId?', (req, res, next) => {
+  try {
+    validateSchema(customerPutContent, req.body);
+  } catch (error) {
+    return next(error);
+  }
+
   res.json({ message: req.body });
+
+  return next();
 });
 
 router.get('/:customerId', (req, res) => {
