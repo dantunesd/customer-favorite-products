313499fa3b6563ecb78b94100ec07bccb8eaa57c
dantunesd <diney.antunes@catho.com>
2021-02-27 17:39:39 -0300

    feat: 🎸 add a string connection

--- src/infrastructure/environment.js
+++ src/infrastructure/environment.js
@@ -1,3 +1,9 @@
-const { APP_NAME, APP_PORT, LOG_LEVEL } = process.env;
+/* eslint-disable object-curly-newline */
+const { APP_NAME, APP_PORT, LOG_LEVEL, CUSTOMERS_DB_URL } = process.env;
 
-module.exports = { APP_NAME, APP_PORT, LOG_LEVEL };
+module.exports = {
+  APP_NAME,
+  APP_PORT,
+  LOG_LEVEL,
+  CUSTOMERS_DB_URL,
+};
