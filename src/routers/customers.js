6f8367c36ddd2b634a8ed93350f658a623a715ef
dantunesd <diney.antunes@catho.com>
2021-02-27 13:34:52 -0300

    feat: 🎸 get customerId from params

--- src/routers/customers.js
+++ src/routers/customers.js
@@ -5,19 +5,25 @@ const customerPutContent = require('../schemas/customerPutContent.json');
 const router = express.Router();
 
 router.put('/:customerId?', (req, res, next) => {
+  const { customerId } = req.params;
+
   try {
     validateSchema(customerPutContent, req.body);
   } catch (error) {
     return next(error);
   }
 
-  res.json({ message: req.body });
+  res.json({ body: req.body, customerId });
 
   return next();
 });
 
-router.get('/:customerId', (req, res) => {
-  res.json({ message: 'hello word' });
+router.get('/:customerId', (req, res, next) => {
+  const { customerId } = req.params;
+
+  res.json({ customerId });
+
+  return next();
 });
 
 module.exports = router;
