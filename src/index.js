df484aef94b82748a877da519bf5114c8eef4c76
dantunesd <diney.antunes@catho.com>
2021-02-28 19:30:28 -0300

    refactor: 💡 adjust path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
-const { environment, logger } = require('./infrastructure');
 const expressApp = require('./api/express-app');
+const environment = require('./infrastructure/environment');
+const logger = require('./infrastructure/logger');
 
 expressApp.listen(environment.APP_PORT, () => {
   logger.info(`API listening at http://localhost:${environment.APP_PORT}`);
