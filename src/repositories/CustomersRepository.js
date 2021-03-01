ffbbac7cf97a3aca5fa09f208a98ecc77f9dff36
dantunesd <diney.antunes@catho.com>
2021-03-01 17:45:46 -0300

    refactor: 💡 simplify repository

--- src/repositories/CustomersRepository.js
+++ src/repositories/CustomersRepository.js
@@ -1,92 +1,68 @@
-/* eslint-disable prettier/prettier */
 const { MongoError, ObjectID } = require('mongodb');
-
 const DuplicatedEmailError = require('../errors/DuplicatedEmailError');
 const CustomerNotFoundError = require('../errors/CustomerNotFoundError');
 
 const duplicateErrorMessage = 'duplicate key error collection';
-
 const dbName = 'customerFavoriteProductsDB';
 const collectionName = 'customersFavoriteProducts';
 
+function isDuplicateKeyError(e) {
+  return e instanceof MongoError && e.message.includes(duplicateErrorMessage);
+}
+
+function handleUpsertError(e) {
+  if (isDuplicateKeyError(e)) {
+    throw new DuplicatedEmailError();
+  }
+  throw e;
+}
+
+function handleCustomerExistence(exists) {
+  if (!exists) {
+    throw new CustomerNotFoundError();
+  }
+}
+
 class CustomersRepository {
   constructor(mongoClient) {
-    this.mongoClient = mongoClient;
+    this.collection = mongoClient.db(dbName).collection(collectionName);
   }
 
   async create(customerData) {
-    const collection = this.mongoClient
-      .db(dbName)
-      .collection(collectionName);
-
-    try {
-      const insertResult = await collection.insertOne(customerData);
-
-      return insertResult.insertedId;
-    } catch (error) {
-      if (
-        error instanceof MongoError
-        && error.message.includes(duplicateErrorMessage)
-      ) {
-        throw new DuplicatedEmailError();
-      }
-      throw error;
-    }
+    return this.collection
+      .insertOne(customerData)
+      .then((result) => result.insertedId)
+      .catch(handleUpsertError);
   }
 
   async getById(customerId) {
-    const collection = this.mongoClient
-      .db(dbName)
-      .collection(collectionName);
+    const filter = { _id: ObjectID(customerId) };
+    const projection = { _id: true, name: true, email: true };
 
-    const findResult = await collection.findOne(
-      { _id: ObjectID(customerId) },
-      { projection: { _id: true, name: true, email: true } },
-    );
-
-    if (!findResult) {
-      throw new CustomerNotFoundError();
-    }
-
-    return findResult;
+    return this.collection.findOne(filter, projection).then((result) => {
+      handleCustomerExistence(result);
+      return result;
+    });
   }
 
   async deleteById(customerId) {
-    const collection = this.mongoClient
-      .db(dbName)
-      .collection(collectionName);
+    const filter = { _id: ObjectID(customerId) };
 
-    const deleteResult = await collection.deleteOne(
-      { _id: ObjectID(customerId) },
-    );
-
-    if (!deleteResult.result.n) {
-      throw new CustomerNotFoundError();
-    }
+    return this.collection.deleteOne(filter).then((result) => {
+      handleCustomerExistence(result.result.n);
+    });
   }
 
   async updateById(customerId, customerData) {
-    const collection = this.mongoClient
-      .db(dbName)
-      .collection(collectionName);
+    const filter = { _id: ObjectID(customerId) };
+    const fields = { $set: customerData };
 
-    try {
-      const updateResult = await collection.updateOne(
-        { _id: ObjectID(customerId) }, { $set: customerData },
-      );
-
-      if (!updateResult.result.n) {
-        throw new CustomerNotFoundError();
-      }
-    } catch (error) {
-      if (
-        error instanceof MongoError
-        && error.message.includes(duplicateErrorMessage)
-      ) {
-        throw new DuplicatedEmailError();
-      }
-      throw error;
-    }
+    return this.collection
+      .updateOne(filter, fields)
+      .then((result) => {
+        handleCustomerExistence(result.result.n);
+      })
+      .catch(handleUpsertError);
   }
 }
 
