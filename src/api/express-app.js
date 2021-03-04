7b38356f8ff20ded5b4143925c2f43d57897e91b
dantunesd <diney.antunes@catho.com>
2021-03-04 18:58:51 -0300

    feat: 🎸 handles not found urls

--- src/api/express-app.js
+++ src/api/express-app.js
@@ -3,6 +3,7 @@ const expressJwt = require('express-jwt');
 const errorHandler = require('./middlewares/errorHandler');
 const requestLogger = require('./middlewares/requestLogger');
 const errorLogger = require('./middlewares/errorLogger');
+const notFoundHandler = require('./middlewares/notFoundHandler');
 const routers = require('./routers');
 const {
   JWT_SECRET,
@@ -24,6 +25,7 @@ app.use(
 app.use('/', routers);
 app.use(errorLogger);
 app.use(errorHandler);
+app.use(notFoundHandler);
 app.use(requestLogger);
 
 module.exports = app;
